5e496dc5ba44c34263c343fab57eb6b713beef49
SrijanSuresh <126934224+SrijanSuresh@users.noreply.github.com>
2024-07-07 21:33:21 -0500

    expanded more css on about me

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+
+
 import './App.css';
 
 function App() {
@@ -42,109 +44,87 @@ function App() {
 
       {view === 'about' && (
         
-        <div className="about-section">
-          <button className="back-button" onClick={() => setView('home')}>Back</button>
-          <div className="about-content">
+          <div className="about-section">
+            
             <div className="about-text">
-              <div className="about-tile">About Me</div>
-              <p className="about-me">
-                Hello! I'm Srijan Suresh, a tech enthusiast who also dabbles in the art of vanquishing virtual dragons and cowboys.
-                When I'm not busy exploring the lands of Elden Ring or Red Dead Redemption 2, I enjoy kicking a soccer ball around, rooting for the golden days of FC Barcelona and Manchester United.<br></br><br></br>
-            
-                On the strategic side, I’m a 1700-rated chess player, regularly battling on the 64 squares.
-                I’ve interned at ACT, where I wrangled student test score data with Tableau and gave the website a fresh new look.
-                Currently, my brain is buzzing with personal projects like finding carpool buddies using Dijkstra’s algorithm and predicting stock prices with a touch of Monte Carlo magic.
-                I’m on a quest for more knowledge and experience in the software field, ready to tackle challenges with a smile and a bit of humor. Let's code, create, and conquer!
-              </p>
-              <div className="badge-container">
-                <div className="badge-base LI-profile-badge"
-                  data-locale="en_US"
-                  data-size="large"
-                  data-theme="light"
-                  data-type="HORIZONTAL"
-                  data-vanity="srijan-suresh"
-                  data-version="v1">
-                  <a className="badge-base__link LI-simple-link"
-                    href="https://www.linkedin.com/in/srijan-suresh?trk=profile-badge">
-                    Srijan Suresh
-                  </a>
-                </div>
-              </div>
-            </div>
-            
-            {/* LinkedIn profile badge */}
-
-            <div className="resume-section">
-            <div className="resume-tile">Digital Resume</div>
-              <div className="resume-content">
-                <p className="resume-text">
-                  {/* Your digital resume content goes here */}
-                  <span className="resume-header">SRIJAN SURESH</span><br />
-            BUFFALO GROVE – IL<br />
-            Phone: 319-259-2055<br />
-            Email: srijansuresh04@gmail.com<br /><br />
-
-            <span className="resume-section-title">Education</span><br />
-            <div className = 'resume-subtitle'><tab>         </tab>University of Illinois at Chicago, <br/><tab>         </tab>Chicago, IL<br />
-            <tab>         </tab>Bachelor of Science in Computer Science (Software Engineering Concentration)<br />
-            <tab>         </tab>August 2023 – December 2025<br /></div>
-            <tab>           </tab>• Dean’s List<br />
-            <tab>           </tab>• Relevant Coursework: Data Structures, Databases, Computer Algorithms, Language and Automation,
-            Applied <br/><tab>             </tab>Statistical Methods, Linear Algebra<br /><br />
-
-            <span className="resume-section-title">Skills</span><br />
-            <div className = 'resume-subtitle'><tab>         </tab>Technical Skills:</div> 
-            <tab>         </tab>C/C++, Python, Java, JavaScript, R, Unix/Linux, HTML, CSS, LATEX<br />
-            <div className = 'resume-subtitle'><tab>         </tab>Database:</div> 
-            <tab>         </tab>PostgreSQL, MySQL, SQLite, NoSQL, Google Firebase<br />
-            <div className = 'resume-subtitle'><tab>         </tab>Frameworks and Libraries: </div>
-            <tab>         </tab>React, Node, Express, Flask, TensorFlow, Pandas, NumPy<br /><br />
-
-            <span className="resume-section-title">Work Experience</span><br />
-              <div className = 'resume-subtitle'>      ACT Software Engineer Intern, Quality Assurance Engineer<br />
-              <tab>         </tab>Iowa City, IA June 2022 – August 2022<br /></div>
+                <button className="back-button" onClick={() => setView('home')}>Back</button> 
+                <div className="about-me">
+                  <div className="about-tile">About Me</div>
+                  Hello! I'm Srijan Suresh, a tech enthusiast who also dabbles in the art of vanquishing virtual dragons and cowboys.
+                  When I'm not busy exploring the lands of Elden Ring or Red Dead Redemption 2, I enjoy kicking a soccer ball around, rooting for the golden days of FC Barcelona and Manchester United.<br></br><br></br>
               
-              <tab>         </tab>• Redesigned the test Score admin page UI/UX and backend, restructuring data accuracy by 25% and decreased
-              <br /><tab>           </tab>administrative processing time.<br />
-              <tab>         </tab>• Utilized Tableau to create interactive and visually appealing dashboards to analyze test score data, identify
-              <br /><tab>           </tab>trends, and present insights to stakeholders.<br />
-              <tab>         </tab>• Refined and executed robust test automation using PyTest.<br />
-              <tab>         </tab>• Launched Tableau to monitor test process performance such as test execution times, defect resolution rates,
-              and <br /><tab>           </tab>test coverage metrics to optimize efficiency and effectiveness.<br /><br />
+                  On the strategic side, I’m a 1700-rated chess player, regularly battling on the 64 squares.
+                  I’ve interned at ACT, where I wrangled student test score data with Tableau and gave the website a fresh new look.
+                  Currently, my brain is buzzing with personal projects like finding carpool buddies using Dijkstra’s algorithm and predicting stock prices with a touch of Monte Carlo magic.
+                  I’m on a quest for more knowledge and experience in the software field, ready to tackle challenges with a smile and a bit of humor. Let's code, create, and conquer!
 
-            <span className="resume-section-title">Personal Projects</span><br />
-              <div className='resume-subtitle'>     CarConnect (April 2024)<br /></div>
-              <tab>         </tab>• Computed a web application that allows users to connect rides with other users in a car share.<br />
-              <tab>         </tab>• Performed Dijkstra’s shortest route algorithm to find nearest user client to connect car, improving efficiency
-              <br /><tab>           </tab>by approximately 40%.<br />
-              <tab>         </tab>• Designed and implemented a React-based front-end UI/UX for dynamic client-side rendering.<br />
-              <tab>         </tab>• Configured Node/Express server backend to manage routing and set up PostgreSQL to store user address
-              and details in <br /><tab>           </tab>database.<br />
-              <tab>         </tab>• Set up a NoSQL Google Firebase to handle user authentication.<br /><br />
-
-              <div className='resume-subtitle'>     SpaceRides (February 2024)<br /></div>
-              <tab>         </tab>• Developed a robust carpooling website in collaboration with 4 people in 24 hours.<br />
-              <tab>         </tab>• Integrated Google Maps API/Leaflet for accurate location tracking and optimized route planning for users.<br />
-              <tab>         </tab>• Programmed site using HTML, CSS, JavaScript and integrated REST API to retrieve user information
-              and process <br /><tab>           </tab>carpooling requests.<br />
-              <tab>         </tab>• Utilized PHP backend to handle user authentication, data storage, and interaction with the REST API,
-              to ensure <br /><tab>           </tab>seamless functionality and security of carpooling site.<br /><br />
-
-              <div className='resume-subtitle'>     Search Algorithm (January 2024)<br /></div>
-              <tab>         </tab>• Programmed a data scraping search algorithm using C++, with several built-in class functions.<br />
-              <tab>         </tab>• Created several test suites using Google Test Suite and increased efficiency by identifying slow searches.<br />
-              <tab>         </tab>• Developed algorithms to remove tokens, filter, and find query matches on 90+ tested articles.<br />
-              <tab>         </tab>• Analyzed key metrics such as precision, recall, and F1 score to assess search algorithm effectiveness.<br />
-              </p>
+                </div>
             </div>
-          </div>
-          </div>
+            <div className="resume-section">
+              <div className="resume-tile">Digital Resume</div>
+                <div className="resume-content">
+                  <p className="resume-text">
+                    {/* Your digital resume content goes here */}
+                    <span className="resume-header">SRIJAN SURESH</span><br />
+                      BUFFALO GROVE – IL<br />
+                      Phone: 319-259-2055<br />
+                      Email: srijansuresh04@gmail.com<br /><br />
 
+                    <span className="resume-section-title">Education</span><br />
+                    <div className = 'resume-subtitle'><tab>         </tab>University of Illinois at Chicago, <br/><tab>         </tab>Chicago, IL<br />
+                    <tab>         </tab>Bachelor of Science in Computer Science (Software Engineering Concentration)<br />
+                    <tab>         </tab>August 2023 – December 2025<br /></div>
+                    <tab>           </tab>• Dean’s List<br />
+                    <tab>           </tab>• Relevant Coursework: Data Structures, Databases, Computer Algorithms, Language and Automation,
+                    Applied <br/><tab>             </tab>Statistical Methods, Linear Algebra<br /><br />
 
-        </div>
+                    <span className="resume-section-title">Skills</span><br />
+                    <div className = 'resume-subtitle'><tab>         </tab>Technical Skills:</div> 
+                    <tab>         </tab>C/C++, Python, Java, JavaScript, R, Unix/Linux, HTML, CSS, LATEX<br />
+                    <div className = 'resume-subtitle'><tab>         </tab>Database:</div> 
+                    <tab>         </tab>PostgreSQL, MySQL, SQLite, NoSQL, Google Firebase<br />
+                    <div className = 'resume-subtitle'><tab>         </tab>Frameworks and Libraries: </div>
+                    <tab>         </tab>React, Node, Express, Flask, TensorFlow, Pandas, NumPy<br /><br />
 
+                    <span className="resume-section-title">Work Experience</span><br />
+                      <div className = 'resume-subtitle'>      ACT Software Engineer Intern, Quality Assurance Engineer<br />
+                      <tab>         </tab>Iowa City, IA June 2022 – August 2022<br /></div>
+                      
+                      <tab>         </tab>• Redesigned the test Score admin page UI/UX and backend, restructuring data accuracy by 25% and decreased
+                      <br /><tab>           </tab>administrative processing time.<br />
+                      <tab>         </tab>• Utilized Tableau to create interactive and visually appealing dashboards to analyze test score data, identify
+                      <br /><tab>           </tab>trends, and present insights to stakeholders.<br />
+                      <tab>         </tab>• Refined and executed robust test automation using PyTest.<br />
+                      <tab>         </tab>• Launched Tableau to monitor test process performance such as test execution times, defect resolution rates,
+                      and <br /><tab>           </tab>test coverage metrics to optimize efficiency and effectiveness.<br /><br />
 
-        
+                    <span className="resume-section-title">Personal Projects</span><br />
+                      <div className='resume-subtitle'>     CarConnect (April 2024)<br /></div>
+                      <tab>         </tab>• Computed a web application that allows users to connect rides with other users in a car share.<br />
+                      <tab>         </tab>• Performed Dijkstra’s shortest route algorithm to find nearest user client to connect car, improving efficiency
+                      <br /><tab>           </tab>by approximately 40%.<br />
+                      <tab>         </tab>• Designed and implemented a React-based front-end UI/UX for dynamic client-side rendering.<br />
+                      <tab>         </tab>• Configured Node/Express server backend to manage routing and set up PostgreSQL to store user address
+                      and details in <br /><tab>           </tab>database.<br />
+                      <tab>         </tab>• Set up a NoSQL Google Firebase to handle user authentication.<br /><br />
+
+                      <div className='resume-subtitle'>     SpaceRides (February 2024)<br /></div>
+                      <tab>         </tab>• Developed a robust carpooling website in collaboration with 4 people in 24 hours.<br />
+                      <tab>         </tab>• Integrated Google Maps API/Leaflet for accurate location tracking and optimized route planning for users.<br />
+                      <tab>         </tab>• Programmed site using HTML, CSS, JavaScript and integrated REST API to retrieve user information
+                      and process <br /><tab>           </tab>carpooling requests.<br />
+                      <tab>         </tab>• Utilized PHP backend to handle user authentication, data storage, and interaction with the REST API,
+                      to ensure <br /><tab>           </tab>seamless functionality and security of carpooling site.<br /><br />
+
+                      <div className='resume-subtitle'>     Search Algorithm (January 2024)<br /></div>
+                      <tab>         </tab>• Programmed a data scraping search algorithm using C++, with several built-in class functions.<br />
+                      <tab>         </tab>• Created several test suites using Google Test Suite and increased efficiency by identifying slow searches.<br />
+                      <tab>         </tab>• Developed algorithms to remove tokens, filter, and find query matches on 90+ tested articles.<br />
+                      <tab>         </tab>• Analyzed key metrics such as precision, recall, and F1 score to assess search algorithm effectiveness.<br />
+                      </p>
+                    </div>
+                 </div>
+             </div>
       )}
 
       {view === 'projects' && (
